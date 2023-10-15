7da49f49d55629edaeb3202dd057a8c176ae45fe
chrismdks <chris.mdks@gmail.com>
2023-10-15 19:34:16 +0300

    add form to add a destination and setup local JSON server

---
+++ src/components/DestinationIndex.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AddDestination from './AddDestination'
+
+function DestinationIndex() {
+  return (
+    <div className='text-white-50'>
+      <h2 className='pt-2 text-success text-center'>Travel List</h2>
+      <AddDestination/>
+    </div>
+  );
+}
+
+export default DestinationIndex
